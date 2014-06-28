65fe698d4bd51efab44175c64098fd9f7388a651
Rafayel Mkrtchyan <rafamian@berkeley.edu>
2014-06-27 17:14:10 -0700

    Update algo.py

--- algo.py
+++ algo.py
@@ -98,7 +98,7 @@ properties:
 		1. The left subtree of a node contains only nodes with keys less than the node’s key.
 		2. The right subtree of a node contains only nodes with keys greater than the node’s key.
 		3. Both the left and right subtrees must also be binary search trees.
-						8
+					    8
 					   / \
 					  4   100
 					 / \   / \
